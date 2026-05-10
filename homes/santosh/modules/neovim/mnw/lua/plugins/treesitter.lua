dcb5fa5534adbdb6039420460d0cd49d53f53c40
vimlinuz <vimlinuz@gmail.com>
2026-05-11 01:53:12 +0545

    nvim:   reduced the lines in the context

--- homes/santosh/modules/neovim/mnw/lua/plugins/treesitter.lua
+++ homes/santosh/modules/neovim/mnw/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 require("treesitter-context").setup({
   enable = true,
   line_numbers = true,
-  max_lines = 5,
+  max_lines = 3,
   min_window_height = 0,
   mode = "cursor",
   multiline_threshold = 20,
