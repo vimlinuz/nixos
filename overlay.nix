f21bff1a663053d4ad5ebf7a98d943265a093018
vimlinuz <vimlinuz@gmail.com>
2026-05-25 18:03:05 +0545

    flake(overlay): added overlay to use the plugs of vim in dev

--- overlay.nix
+++ overlay.nix
@@ -2,14 +2,27 @@
 {
   nixpkgs = {
     overlays = [
-      (final: prev: {
-        vimPlugins = prev.vimPlugins // {
-          black-metal-theme-neovim = prev.vimUtils.buildVimPlugin {
-            name = "black-metal-theme-neovim";
-            src = inputs.black-metal-theme-neovim;
+      (
+        final: prev:
+        let
+          generated = (
+            (final.callPackage /home/santosh/dev/nixpkgs/pkgs/applications/editors/vim/plugins/generated.nix {
+              buildVimPlugin = prev.vimUtils.buildVimPlugin;
+            })
+              final
+              prev
+          );
+        in
+        {
+          vimPlugins = prev.vimPlugins // {
+            black-metal-theme-neovim = prev.vimUtils.buildVimPlugin {
+              name = "black-metal-theme-neovim";
+              src = inputs.black-metal-theme-neovim;
+            };
+            blink-calc = generated.blink-calc;
           };
-        };
-      })
+        }
+      )
     ];
   };
 }
