06fe01aae7604596f6e21d02d5aa1ca8e6f876e9
vimlinuz <vimlinuz@gmail.com>
2026-05-28 16:25:34 +0545

    pkgs(fzf): added config for dir opts and colors

--- homes/santosh/modules/fzf.nix
+++ homes/santosh/modules/fzf.nix
@@ -2,5 +2,13 @@
   programs.fzf = {
     enable = true;
     enableBashIntegration = true;
+    changeDirWidgetCommand = "fd --type d";
+    changeDirWidgetOptions = [
+      "--preview 'tree -C {} | head -200'"
+    ];
+    colors = {
+      pointer = "#ffffff";
+      marker = "#000000";
+    };
   };
 }
