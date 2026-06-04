3f34cd6aa1d6bb522ce6486896963bd572a7ba28
vimlinuz <vimlinuz@gmail.com>
2026-06-04 20:56:05 +0545

    tmux(tmux-sm): used the nixpkgs version of tmux-sm

--- homes/santosh/modules/tmux/modules/plugins/tmux-sm.nix
+++ homes/santosh/modules/tmux/modules/plugins/tmux-sm.nix
@@ -1,20 +1,6 @@
 { pkgs, ... }:
-let
-  tmux-sm = pkgs.tmuxPlugins.mkTmuxPlugin {
-    pluginName = "tmux-sm";
-    rtpFilePath = "main.tmux";
-    version = "unstable-2026-04-03";
-    src = pkgs.fetchFromGitHub {
-      owner = "vimlinuz";
-      repo = "tmux-sm";
-      rev = "97d411a11d124443c982d17fde03c1e09809d7b1";
-      hash = "sha256-7HW/TLP/yyQp4j0/utA0tibTv+suV1B2K56pUS3Z004=";
-    };
-  };
-
-in
 {
-  plugin = tmux-sm;
+  plugin = pkgs.tmuxPlugins.tmux-sm;
   extraConfig = ''
     set -g @sessionizer_key '-n M-i'
     set -g @sessionizer_height '70%'
