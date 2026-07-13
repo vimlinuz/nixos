a44c9faf1ddfdc2d20cad67e20632a2bfb4a9e16
vimlinuz <vimlinuz@gmail.com>
2026-07-14 00:10:04 +0545

    fix(scripts): fixed the syntax issue

--- homes/santosh/modules/scripts/pull.nix
+++ homes/santosh/modules/scripts/pull.nix
@@ -4,30 +4,29 @@
   ...
 }:
 let
-  yt-dlp = "${lib.getExe pkgs.yt-dlp}";
+  yt-dlp = lib.getExe pkgs.yt-dlp;
   pull = pkgs.writeShellScriptBin "pull" ''
-    #!/usr/bin/env bash
-
     URL="$1"
     MODE="$2"
 
-    if [[ "$MODE" == "--help" ]] || [[ -z "$URL" ]] || [[ "$URL" == "--help" ]] || [[ "$URL" == "--music" ]] || [[ "$URL" == "--video" ]]; then
-      echo "Usage: pull <url> <mode>"
-      echo "Modes:"
-      echo "  --music: download music"
-      echo "  --video: download video"
-      exit 0;
+    if [[ "$MODE" == "--help" ]] || [[ -z "$URL" ]] || [[ "$URL" == "--help" ]]; then
+      echo "Usage: pull <url> [--music|--video]"
+      exit 0
     fi
 
     if [[ "$MODE" == "--music" ]]; then
       ${yt-dlp} -x --audio-format mp3 "$URL"
-      exit 0;
+      exit 0
     fi
 
     if [[ "$MODE" == "--video" ]] || [[ -z "$MODE" ]]; then
       ${yt-dlp} --merge-output-format mp4 -f "bestvideo+bestaudio/best" "$URL"
-      exit 0;
+      exit 0
     fi
+
+    echo "Unknown mode: $MODE"
+    echo "Use --help for usage."
+    exit 1
   '';
 in
 {
