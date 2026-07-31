551176983017c07a1ab5e08c30423ea5ecbbcb5a
vimlinuz <vimlinuz@gmail.com>
2026-07-31 16:40:12 +0545

    fzf: update the colors of the fzf to vague

--- homes/santosh/modules/fzf.nix
+++ homes/santosh/modules/fzf.nix
@@ -10,9 +10,20 @@
       "--preview 'tree -C {} | head -200'"
     ];
     colors = {
-      pointer = "#ffffff";
-
-      marker = "#000000";
+      fg = "#cdcdcd";
+      bg = "#141415";
+      hl = "#f3be7c";
+      "fg+" = "#aeaed1";
+      "bg+" = "#252530";
+      "hl+" = "#f3be7c";
+      border = "#606079";
+      header = "#6e94b2";
+      gutter = "#141415";
+      spinner = "#7fa563";
+      info = "#f3be7c";
+      pointer = "#aeaed1";
+      marker = "#d8647e";
+      prompt = "#bb9dbd";
     };
     defaultOptions = [
       "--reverse"
